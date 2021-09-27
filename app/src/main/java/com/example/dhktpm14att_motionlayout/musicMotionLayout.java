5ebcd1fb5290f4983e38e5aada115c8223048013
DESKTOP-1VFJKRB\USER <ngtbao2000@gmail.com>
2021-09-27 19:28:30 +0700

    finish music motionlayout

---
+++ app/src/main/java/com/example/dhktpm14att_motionlayout/musicMotionLayout.java
@@ -0,0 +1,14 @@
+package com.example.dhktpm14att_motionlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class musicMotionLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_motion_layout);
+    }
+}
